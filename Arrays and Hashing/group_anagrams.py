ca907cc57bb7703a233585ed54477c08cc6b20a7
Arturo Hinojosa <arturohin_16@outlook.com>
2023-04-07 13:01:37 -0600

    Group Anagrams Problem

--- Arrays and Hashing/group_anagrams.py
+++ Arrays and Hashing/group_anagrams.py
@@ -1,19 +1,25 @@
 """
 Given an array of strings strs, group the anagrams together. You can return the answer in any order
 
+strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
 SOLUTION
 ord() is a built in function that returns the Unicode code point of a Unicode character.
+Time Complexity O(n)
+Space Complexity O(n)
 """
 from collections import defaultdict
 def groupAnagrams(strs):
-    ans = defaultdict(list)
+    groups = defaultdict(list)
     for s in strs:
-        count = [0] * 26
+        ans = [0] * 26 
         for c in s:
-            count[ord(c) - ord("a")] += 1 # Value used as an index into an array to access the element. 
-        ans[tuple(count)].append(s)
-    return ans.values()
+            ans[ord(c) - ord('a')] += 1 # Value used as an index into an array to access the element. 
+        groups[tuple(ans)].append(s)
+    return groups.values()
+        
+
 
 strs = ["eat","tea","tan","ate","nat","bat"]
 print(groupAnagrams(strs))
-#Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
