45ff4b803a1967b2628a7d7bbb3bf9f0c645b412
Arturo Hinojosa <arturohin_16@outlook.com>
2023-04-18 22:03:34 -0600

    Se hacen 3 nuevos ejercicios de Stack

---
+++ Stack/evaluate_polish_notation.py
@@ -0,0 +1,36 @@
+"""
+You are given an array of strings tokens that represents an arithmetic expression in a Reverse Polish Notation.
+Evaluate the expression. Return an integer that represents the value of the expression.
+
+Note that:
+    - The valid operators are '+', '-', '*', and '/'.
+    - Each operand may be an integer or another expression.
+    - The division between two integers always truncates toward zero.
+    - There will not be any division by zero.
+    - The input represents a valid arithmetic expression in a reverse polish notation.
+    - The answer and all the intermediate calculations can be represented in a 32-bit integer.
+"""
+def evalRPN(tokens):
+    stack = []
+    for token in tokens:
+        if token in ['+','-','*','/']:
+            value1 = stack.pop()
+            value2 = stack.pop() 
+            if token == '+':
+                total = value1 + value2
+                stack.append(total)
+            elif token == '-':
+                total = value2 - value1
+                stack.append(total)
+            elif token == '*':
+                total = value1 * value2
+                stack.append(total)
+            elif token == '/':
+                total = int(value2/ value1)
+                stack.append(total)
+        else:
+            stack.append(int(token))
+    return stack[0]
+
+tokens = ["4","13","5","/","+"]
+print(evalRPN(tokens))
--- Stack/valid_parentheses.py
+++ Stack/valid_parentheses.py
@@ -1,18 +1,26 @@
-def isValid(s):
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine 
+if the input string is valid. An input string is valid if:
+    - Open brackets must be closed by the same type of brackets.
+    - Open brackets must be closed in the correct order.
+    - Every close bracket has a corresponding open bracket of the same type.
+"""
+def isValid(parenthesis):
     hashmap = {
         ')':'(',
         '}':'{',
         ']':'['
     }
     stack = []
-    for c in s:
-        if c in hashmap:
-            if stack or hashmap[c] == stack[-1]:
+    for char in parenthesis: 
+        if char in hashmap:
+            if stack and hashmap[char] == stack[-1]:
                 stack.pop()
             else:
                 return False
         else:
-            stack.append(c)
+            stack.append(char)
     return True if not stack else False
-s = '()[]('
-print(isValid(s))
+
+parenthesis = '()[]()'
+print(isValid(parenthesis))
