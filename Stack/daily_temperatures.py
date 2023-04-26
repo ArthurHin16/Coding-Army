145ddbd431ece6a37520e083af4b62fe1a87746a
Arturo Hinojosa <arturohin_16@outlook.com>
2023-04-26 16:31:44 -0600

    Daily Temperatures

--- Stack/daily_temperatures.py
+++ Stack/daily_temperatures.py
@@ -3,16 +3,14 @@ Given an array of integers temperatures represents the daily temperatures, retur
 answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. 
 If there is no future day for which this is possible, keep answer[i] == 0 instead.
 """
-#Monotonic Decreasing Stack
 def dailyTemperatures(temperatures):
     result = [0] * len(temperatures)
-    stack = [] # pair: [temp, index]
-    for i, t in enumerate(temperatures):
-        while stack and t > stack[-1][0]: 
-            stackT, stackInd = stack.pop()
-            result[stackInd] = (i - stackInd)
-        stack.append([t,i])
+    stack = [] #The stack will have a pair value [temperature, index]
+    for index, temp  in enumerate(temperatures): #The enumerate() method returns the index, value
+        while stack and temp > stack[-1][0]:
+            stackT, stackI = stack.pop()
+            result[stackI] = ( index - stackI )
+        stack.append([temp, index]) #Monotonic Decreasing Stack
     return result
-
 temperatures = [73,74,75,71,69,72,76,73]
 print(dailyTemperatures(temperatures))
